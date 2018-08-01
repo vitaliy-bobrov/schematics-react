9b5d709470032343b35ef4e22d8d2c28329b2791
Vitaliy Bobrov <bobrov1989@gmail.com>
2018-08-01 16:35:21 +0200

    Change scehma ext

---
+++ src/component/schema.ts
@@ -0,0 +1,34 @@
+export interface Schema {
+  /**
+   * The path to create the component.
+   */
+  path?: string;
+  /**
+   * The name of the component.
+   */
+  name: string;
+  /**
+   * The file extension to be used for style files.
+   */
+  styleext?: string;
+  /**
+   * Specifies if a spec file is generated.
+   */
+  spec?: boolean;
+  /**
+   * Flag to indicate if a dir is created.
+   */
+  flat?: boolean;
+  /**
+   * Specifies if a propTypes used.
+   */
+  propTypes?: boolean;
+  /**
+   * Specifies if a state used.
+   */
+  stateful?: boolean;
+  /**
+   * Specifies whether to use TypeScript.
+   */
+  ts?: boolean;
+}
